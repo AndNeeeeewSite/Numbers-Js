c273c2ac34a0eb99aecae88660e315e204a8a0c9
AndNeeeeewSite <pavlopysarenko1234@gmail.com>
2024-07-25 11:17:08 +0300

    !

---
+++ js/script.js
@@ -0,0 +1,37 @@
+//  1.
+let C = 36.6
+let F = C*9/5+32
+console.log("1. " + F + " F")
+//   2.
+let monthDays = 31
+let monthHour = 31 * 24
+let monthMinutes = monthHour * 60
+console.log("2. Днів: " + monthDays + ", Годин: " + monthHour + ", Хвилин: " + monthMinutes)
+//   3.
+let healt = 100 - 20
+let energy = 120 - 50
+console.log("Здоров'я: "+ healt + ", Енергія: "+ energy)
+//   4.
+let total = 20000
+console.log("Сума покупки: " + total + " -10% = " + total * 0.9)
+//   5.
+let p = 3.141592653589793238462643
+p = Math.floor(p)
+console.log(p)
+//   6.
+let floatString = "123.456qwe123rty";
+let floatNumber = parseFloat(floatString);
+console.log(floatNumber);
+//   7.
+let floatString2 = "123qwe123rty";
+let floatNumber2 = parseInt(floatString2);
+console.log(floatNumber2);
+//   8.
+let sqrtNumber = 9
+let sqrt = Math.sqrt(sqrtNumber) 
+console.log(sqrt)
+//   9.
+let FullNumber = 12345
+let lineNumber = '123'
+console.log(Number.parseInt(lineNumber))
+console.log(FullNumber.toString())
